4161aac4ebe85208cfba16e52f9db49b7634d7a0
Aloun <alounv@gmail.com>
2023-01-27 14:47:50 +0100

    Decode req.body

--- api/choices.rs
+++ api/choices.rs
@@ -1,18 +1,34 @@
 use http::StatusCode;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use vercel_lambda::{error::VercelError, lambda, IntoResponse, Request, Response};
 
-fn handler(_: Request) -> Result<impl IntoResponse, VercelError> {
+#[derive(Deserialize)]
+struct ParsedBody {
+    test: String,
+}
+
+#[derive(Serialize)]
+struct Data {
+    data: String,
+}
+
+fn handler(req: Request) -> Result<impl IntoResponse, VercelError> {
+    let body = req.into_body();
+    let input: ParsedBody = serde_json::from_slice(&body).unwrap();
+
+    let results = Data { data: input.test };
+
+    let data = serde_json::json!(results);
     let response = Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", "text/plain")
-        .body("Hello World")
+        .body(data.to_string())
         .expect("Internal Server Error");
 
     Ok(response)
 }
 
-// Start the runtime with the handler
 fn main() -> Result<(), Box<dyn Error>> {
     Ok(lambda!(handler))
 }
