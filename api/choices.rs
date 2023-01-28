28b167bd013c0c78e5972abf7fc4d199fa1f07d5
Aloun <alounv@gmail.com>
2023-01-28 20:00:35 +0100

    Manage wrong API input

--- api/choices.rs
+++ api/choices.rs
@@ -6,18 +6,29 @@ pub mod calculations;
 
 fn handler(req: Request) -> Result<impl IntoResponse, VercelError> {
     let body = req.into_body();
-    let input: calculations::Input = serde_json::from_slice(&body).unwrap();
+    let input = serde_json::from_slice(&body);
 
-    let results = calculations::main(&input).unwrap();
+    match input {
+        Ok(input) => {
+            let results = calculations::main(&input).unwrap();
 
-    let data = serde_json::json!(results);
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "text/plain")
-        .body(data.to_string())
-        .expect("Internal Server Error");
+            let data = serde_json::json!(results);
+            let response = Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", "text/plain")
+                .body(data.to_string())
+                .expect("Internal Server Error");
 
-    Ok(response)
+            Ok(response)
+        }
+        Err(e) => {
+            let error = format!("Error: {}", e);
+            Ok(Response::builder()
+                .status(StatusCode::BAD_REQUEST)
+                .body(error)
+                .unwrap())
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
