360d5d94572e10d0b432c7074448aefe7b198ae7
Aloun <alounv@gmail.com>
2023-01-28 19:56:24 +0100

    Separate in various files

--- api/calculations/mod.rs
+++ api/calculations/mod.rs
@@ -1,7 +1,9 @@
-use rand::Rng;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error::Error;
+
+pub mod apply_choice;
+pub mod get_next_person;
 
 #[derive(Deserialize)]
 pub struct Input {
@@ -22,48 +24,8 @@ pub struct Person {
     assigned: Vec<String>,
 }
 
-// Next person to choose a project should :
-// 1. have the lowest number of storypoints already assigned
-// 2. in case of equality, have the biggest number of denials
-// 3. in case of equality, be randomly choosen
-fn get_next_person(state: &mut HashMap<String, Person>) -> String {
-    let mut next_person = String::new();
-
-    let mut min_points = 1000;
-    let mut max_denials = 0;
-    let mut candidates = Vec::new();
-
-    for (name, person_data) in state.iter() {
-        let points = person_data.points;
-        let denials = person_data.denials;
-        if points < min_points {
-            min_points = points;
-            max_denials = denials;
-            next_person = name.clone();
-        } else if points == min_points {
-            if denials > max_denials {
-                max_denials = denials;
-                next_person = name.clone();
-            } else if denials == max_denials {
-                candidates.push(name.clone());
-            }
-        }
-    }
-
-    if candidates.len() > 0 {
-        let mut rng = rand::thread_rng();
-        let index = rng.gen_range(0..candidates.len());
-        next_person = candidates[index].clone();
-    }
-
-    next_person
-}
-
-pub fn main(input: &Input) -> Result<Data, Box<dyn Error>> {
-    let mut projects = input.projects.clone();
-    let mut steps = vec![];
-    let mut state = input
-        .wishes
+fn get_initial_state(wishes: &HashMap<String, Vec<String>>) -> HashMap<String, Person> {
+    wishes
         .iter()
         .map(|(name, wishes)| {
             (
@@ -76,45 +38,19 @@ pub fn main(input: &Input) -> Result<Data, Box<dyn Error>> {
                 },
             )
         })
-        .collect::<HashMap<String, Person>>();
+        .collect::<HashMap<String, Person>>()
+}
+
+pub fn main(input: &Input) -> Result<Data, Box<dyn Error>> {
+    let mut projects = input.projects.clone();
+    let mut steps = vec![];
+    let mut state = get_initial_state(&input.wishes);
 
     for _i in 0..projects.len() {
-        let next_person_name: String = get_next_person(&mut state);
-        let next_person = state.get(&next_person_name).unwrap();
-        let next_person_wishes = next_person.wishes.clone();
-        let next_person_assigned = next_person.assigned.len();
-
-        if next_person_wishes.len() == 0 {
-            break;
-        }
-
-        let project = next_person_wishes[0].clone();
-
-        projects.remove(&project);
-        state.iter_mut().for_each(|(name, person)| {
-            if name == &next_person_name {
-                person.assigned.push(project.clone());
-                person.points += input.projects.get(&project).unwrap();
-            } else if person.wishes.len() > 0
-                && person.assigned.len() <= next_person_assigned
-                && person.wishes[0] == project
-            {
-                println!("XXX {} denied to {} by {}", project, name, next_person_name);
-                person.denials += 1;
-            }
-            // We add a denial each time the person first choice is taken by someone else
-            // Who had not less projects than him
-
-            person.wishes.retain(|w| w != &project);
-        });
-
-        // println!(" ===>>> {} took {}", next_person_name, project);
-        // println!("{:#?}", state);
-        // println!();
-
-        steps.push(state.clone());
-
-        if projects.len() == 0 {
+        let next_person_name: String = get_next_person::main(&mut state);
+        let should_break =
+            apply_choice::main(next_person_name, &mut state, &mut projects, &mut steps);
+        if should_break {
             break;
         }
     }
