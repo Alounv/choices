8270ce3bf0ebc3833e56419c849299630ece898f
Aloun <alounv@gmail.com>
2023-01-29 22:25:33 +0100

    Improves result display

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,16 +1,85 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import type { State } from "~/components/types";
 
 export default component$(({ state }: { state: State }) => {
+  const { results, isLoading, reqDuration } = state;
+  const local = useStore({ stepIndex: 0 });
+  const currentStep = results[local.stepIndex] || {};
+  const users = Object.keys(currentStep);
+  const firstWishes = users.map((user) => ({
+    user,
+    project: currentStep[user].wishes[0],
+  }));
+
   return (
     <>
       <h2>Results</h2>
       <div>
-        {state.isLoading
-          ? "LOADING..."
-          : `Request duration: ${state.reqDuration}ms`}
+        {isLoading ? `Loading...` : `Request duration: ${reqDuration}ms`}
       </div>
-      <div>{state.results.length > 0 && JSON.stringify(state.results)}</div>
+
+      <div class="flex gap-2">
+        {results.map((_, index) => {
+          const isSelected = index === local.stepIndex;
+          return (
+            <button
+              class={`bg-slate-100 p-2 border-2 rounded-md ${
+                isSelected ? "bg-blue-200" : ""
+              }`}
+              onClick$={() => {
+                local.stepIndex = index;
+              }}
+            >
+              {index}
+            </button>
+          );
+        })}
+      </div>
+
+      {results.length > 0 && (
+        <ul class="flex flex-col gap-4">
+          {users.map((name) => {
+            const { denials, wishes, points, assigned } = currentStep[name];
+            wishes.reverse();
+            return (
+              <li class="flex gap-2 items-center">
+                <span class="font-bold">{name}</span>
+                <span> {Array(points).fill("⚬").join("")}</span>
+                <span class="text-xs">
+                  {Array(denials).fill("🚫").join("")}
+                </span>
+                <span class="flex-1" />
+                {wishes.map((w, i) => {
+                  const isLast = i === wishes.length - 1;
+                  const otherFirstWishes = firstWishes
+                    .filter((l) => l.user !== name)
+                    .map((l) => l.project);
+                  const isDisputed = isLast && otherFirstWishes.includes(w);
+                  return (
+                    <span
+                      class={`border-2 px-1 ${isLast ? "font-bold" : ""} ${
+                        isDisputed ? "border-red-700 bg-red-700 text-white" : ""
+                      } `}
+                    >
+                      {w}
+                    </span>
+                  );
+                })}
+                <span> ➡️ </span>
+                {assigned.map((a) => {
+                  return (
+                    <span
+                      class={`border-2 text-white px-1 border-sky-500 bg-sky-500`}
+                    >
+                      {a}
+                    </span>
+                  );
+                })}
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,13 +8,145 @@ import type { State } from "~/components/types";
 export default component$(() => {
   const state: State = useStore({
     input: {
-      wishes: { Alice: ["A", "B"] },
-      projects: { A: 1, B: 2 },
+      wishes: {
+        Alice: ["A", "D", "F", "C", "B", "E"],
+        Bob: ["B", "C", "E", "A", "D", "F"],
+        Charlie: ["C", "A", "B", "E", "F", "D"],
+      },
+      projects: {
+        A: 1,
+        B: 2,
+        C: 3,
+        D: 4,
+        E: 5,
+        F: 6,
+      },
     },
     reqDuration: 0,
     inputError: null,
     isLoading: false,
-    results: [],
+    results: [
+      {
+        Alice: {
+          assigned: [],
+          denials: 0,
+          points: 0,
+          wishes: ["E", "C", "F", "D", "A"],
+        },
+        Bob: {
+          assigned: ["B"],
+          denials: 0,
+          points: 2,
+          wishes: ["F", "D", "A", "E", "C"],
+        },
+        Charlie: {
+          assigned: [],
+          denials: 0,
+          points: 0,
+          wishes: ["D", "F", "E", "A", "C"],
+        },
+      },
+      {
+        Alice: {
+          assigned: ["A"],
+          denials: 0,
+          points: 1,
+          wishes: ["D", "F", "C", "E"],
+        },
+        Bob: {
+          assigned: ["B"],
+          denials: 0,
+          points: 2,
+          wishes: ["C", "E", "D", "F"],
+        },
+        Charlie: {
+          assigned: [],
+          denials: 0,
+          points: 0,
+          wishes: ["C", "E", "F", "D"],
+        },
+      },
+      {
+        Alice: {
+          assigned: ["A"],
+          denials: 0,
+          points: 1,
+          wishes: ["D", "F", "E"],
+        },
+        Bob: {
+          assigned: ["B"],
+          denials: 0,
+          points: 2,
+          wishes: ["E", "D", "F"],
+        },
+        Charlie: {
+          assigned: ["C"],
+          denials: 0,
+          points: 3,
+          wishes: ["E", "F", "D"],
+        },
+      },
+      {
+        Alice: {
+          assigned: ["A", "D"],
+          denials: 0,
+          points: 5,
+          wishes: ["F", "E"],
+        },
+        Bob: {
+          assigned: ["B"],
+          denials: 0,
+          points: 2,
+          wishes: ["E", "F"],
+        },
+        Charlie: {
+          assigned: ["C"],
+          denials: 0,
+          points: 3,
+          wishes: ["E", "F"],
+        },
+      },
+      {
+        Alice: {
+          assigned: ["A", "D"],
+          denials: 0,
+          points: 5,
+          wishes: ["F"],
+        },
+        Bob: {
+          assigned: ["B", "E"],
+          denials: 0,
+          points: 7,
+          wishes: ["F"],
+        },
+        Charlie: {
+          assigned: ["C"],
+          denials: 1,
+          points: 3,
+          wishes: ["F"],
+        },
+      },
+      {
+        Alice: {
+          assigned: ["A", "D"],
+          denials: 0,
+          points: 5,
+          wishes: [],
+        },
+        Bob: {
+          assigned: ["B", "E"],
+          denials: 0,
+          points: 7,
+          wishes: [],
+        },
+        Charlie: {
+          assigned: ["C", "F"],
+          denials: 1,
+          points: 9,
+          wishes: [],
+        },
+      },
+    ],
   });
 
   return (
